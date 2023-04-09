d0de4667b292f78b491dc42a7e6ab9b8b7a173ea
Fitzhavey <michael.fitzhavey@gmail.com>
2023-04-08 23:38:01 -0700

    fix: debug mode works again

--- scripts/narration.py
+++ scripts/narration.py
@@ -1,11 +1,20 @@
 from tts_engines.google_tts import narrate as narrate_google
 from tts_engines.eleven_labs import narrate as narrate_eleven
+import shutil
+from config import Config
+cfg = Config()
 
-def create_narration(input_script, options, output_path):
+def create_narration(input_script, options, output_dir):
+
+    if cfg.debug:
+        output_path = output_dir + "/narration.mpeg"
+        shutil.copy("lib/voice-sample.mpeg", output_path)
+
+
     engine = options["engine"]
     if engine == "google":
-        return narrate_google(input_script, output_path)
+        return narrate_google(input_script, output_dir)
     elif engine == "eleven":
-        return narrate_eleven(input_script, output_path)
+        return narrate_eleven(input_script, output_dir)
     else:
         raise Exception("Unknown engine: " + engine)
