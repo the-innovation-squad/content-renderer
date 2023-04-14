e72f5aab1ba61c3efe106d402f8f6ec225e411e8
Fitzhavey <michael.fitzhavey@gmail.com>
2023-04-14 13:54:05 -0400

    chore: better func name

--- scripts/config.py
+++ scripts/config.py
@@ -1,6 +1,6 @@
 import os
 
-def get_env_var(key):
+def safe_load_env_var(key):
     value = os.getenv(key)
     if value is None:
         raise Exception(f"Environment variable {key} is not set")
@@ -8,5 +8,5 @@ def get_env_var(key):
 
 config = {
     "debug": False,
-    "ELEVENLABS_API_KEY": get_env_var("ELEVENLABS_API_KEY")
+    "ELEVENLABS_API_KEY": safe_load_env_var("ELEVENLABS_API_KEY")
 }
